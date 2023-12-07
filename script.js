6e4a35f037f940b9f955f7651336fe0ad3520e4f
Deepak Kanshetti <kanshettid@gmail.com>
2023-12-07 22:52:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 //your code here
 
+function swapTheme() {
+  // Get the main div and button by their IDs
+  var appDiv = document.getElementById("app");
+  var swapButton = document.getElementById("swap");
+
+  // Toggle between 'day' and 'night' classes on the main div
+  appDiv.classList.toggle("day");
+  appDiv.classList.toggle("night");
+
+  // Toggle between 'button_day' and 'button_night' classes on the button
+  swapButton.classList.toggle("button_day");
+  swapButton.classList.toggle("button_night");
+}
